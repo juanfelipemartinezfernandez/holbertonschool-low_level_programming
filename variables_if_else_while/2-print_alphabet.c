764defdec2292ec63bbef439472f513ed7fa7e40
Juan Felipe <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 14:20:55 -0700

    Added change

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for(alp = 'a'; alp <='z'; alp++)
+	{
+		putchar(alp);
+	}
+	
+	return(0);
+}
