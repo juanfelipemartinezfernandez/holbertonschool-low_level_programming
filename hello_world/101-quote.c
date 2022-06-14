6a580e0952e60737176f55f990134595d338b860
Juan Felipe <juanfelipe.martinez.fernandez@gmail.com>
2022-06-13 19:07:12 -0700

    Added change

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+return (1);
+}
