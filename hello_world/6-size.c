5e61b311fc82ea9137d1a1cce15dde48a7667d86
Juan Felipe <juanfelipe.martinez.fernandez@gmail.com>
2022-06-13 18:55:08 -0700

    Added change

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ int n2;
 long int n3;
 long long int n4;
 float n5;
-printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(n1));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(n2));
-printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(n3));
-printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(n4));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(n5));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(n1));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(n2));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n3));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(n4));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(n5));
 return (0);
 }
