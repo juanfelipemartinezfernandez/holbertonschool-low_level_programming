710632b640ea40643c8c3013ac23cd31ef50c0f9
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 17:24:06 -0500

    Create 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+        for(n=0; n<10; n++)
+        {
+            char c = n + '0';
+            putchar(c);
+        }
+        
+        putchar('\n');
+
+        return(0);
+
+}
