a021f4361f1fa73b3e8cd42f2fb54ec24f5695d6
Juan Felipe <juanfelipe.martinez.fernandez@gmail.com>
2022-06-13 18:33:57 -0700

    Added change

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Main - entry point
+ * Using printf to print message
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
