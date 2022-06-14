1e29361b4e7d0a5fdc150b4142822b417bdfed6d
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 17:32:03 -0500

    Create 8-print_base16.c
    Added 2 files

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int n;
+
+        for(n=0; n<17; n++)
+        {
+            
+            printf("%x", n);
+        }
+        
+        putchar('\n');
+
+        return(0);
+}
