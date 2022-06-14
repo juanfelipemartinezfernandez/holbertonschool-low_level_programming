442f3ce65bc8badc69213efe977c3e424dc58c8f
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 18:29:45 -0500

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,15 +12,19 @@ int n;
 
         for(n=0; n<10; n++)
         {
-            if (n != 9){
-                putchar(n+'0');
+            putchar(n+'0');
+            
+            if (n != 9){      
+
                 putchar(',');
-                putchar(' ');
+                
             }
             else
             {
+
                 putchar(n+'0');
             }
+            putchar(' ');
         }
         
         
