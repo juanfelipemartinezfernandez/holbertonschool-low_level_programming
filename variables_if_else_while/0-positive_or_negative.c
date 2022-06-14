fda38591dd5ce078cfc74ef30495c32e3cf7ceba
Juan Felipe <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 12:29:36 -0700

    Added change

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,15 @@
 #include <time.h>
 /**
  * main - Entry point
- * Description: 'print whether the number stored 
- * in the variable n is positive or negative' 
+ * Description: 'print whether the number stored
+ * in the variable n is positive or negative'
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
