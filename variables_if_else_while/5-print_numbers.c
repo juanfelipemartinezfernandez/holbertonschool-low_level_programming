3c1ffdac7a6915d3884445e0b6619aff0555f21e
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 16:49:08 -0500

    Create 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+for(int n=0; n<10; n++)
+{
+    puts(n);
+}
+putchar('\n');
+return(0);
+}
