f8af56a6d5cfb4b2a06b5b3f7a42b14f5a0a9fbe
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 18:06:37 -0500

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,6 +8,21 @@
 
 int main(void)
 {
+int n,i;
 
-return(0);
+        for(n=0; n<10; n++)
+        {
+            for(i=0; i<10; i++)
+            {
+            
+                putchar(n+'0');
+                putchar(i+'0');
+            }
+            
+        }
+        
+        putchar('\n');
+
+        return(0);
+
 }
