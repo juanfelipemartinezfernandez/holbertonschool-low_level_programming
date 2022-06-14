136f365e0a98153d6e32daf465e628a4f7098e02
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-14 18:48:55 -0500

    Update 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	char alp;
-	for(alp = 'a'; alp <='z'; alp++)
-	{
-		putchar(alp);
-	}
-	putchar('\n');
-	return(0);
+char alp;
+for(alp = 'a'; alp <='z'; alp++)
+{
+putchar(alp);
+}
+putchar('\n');
+return(0);
 }
