c955ea466f62f4b9f13f30e4426789fa9fae8beb
Juan Felipe <juanfelipe.martinez.fernandez@gmail.com>
2022-06-13 18:48:56 -0700

    Added change

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Print size of types'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char n1;
+  int n2;
+  long int n3;
+  long long int n4;
+  float n5;
+  printf("Size of a char: %lu",(unsigned long)sizeof(n1));
+  printf("Size of a int: %lu",(unsigned long)sizeof(n2));
+  printf("Size of a long int: %lu",(unsigned long)sizeof(n3));
+  printf("Size of a long long int: %lu",(unsigned long)sizeof(n4));
+  printf("Size of a float: %lu",(unsigned long)sizeof(n5));
+}
