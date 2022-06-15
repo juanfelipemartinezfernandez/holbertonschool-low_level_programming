a7d76bcdd6070e2fee17d668db5a35c009b7b69f
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-15 14:20:06 -0500

    Update 8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,15 +2,15 @@
 
 void jack_bauer(void)
 {
-    char h,m;
+    int h,m;
 
-    for ( h= '0'; h <='24'; h++)
+    for ( h= 0; h <=24; h++)
     {
         if(h <= 9)
         _putchar('0');
         _putchar(h);
         _putchar(':');
-        for (m = '0'; m<='59'; m++)
+        for (m = 0; m<=59; m++)
         {
         if(m <= 9)
         _putchar('0');
