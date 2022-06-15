fce1fc031d2976c7f398690e9291b34c4aa83617
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-15 13:59:07 -0500

    added

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+
+void print_to_98(int n)
+{
+
+    
+}
---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int _abs(int n)
+{
+    int absolute;
+
+    if (n >= 0)
+    return (n);
+    
+    return ((absolute=n*-1));
+
+
+}
