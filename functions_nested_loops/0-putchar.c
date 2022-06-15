0352b88e2ac9c1d4506b3230c371895f04a01b8c
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-15 12:23:11 -0500

    Create 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*Description: 'What programm does'
+*Return: Always 0 (success)
+*/
+void print();
+
+int main(void)
+{
+print();
+return (0);
+}
+
+void print()
+{
+printf("putchar");
+return;
+}
