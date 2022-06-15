07563baa66baf04a31688f82be3e15d82d83a455
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-15 16:23:24 -0500

    Update 10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  *               in order separated by a comma followed by a space.
  * @num1: The number to begin counting at.
  * @num2: The number to begin counting at.
- * return: the sum of both numbers
+ * Return: the sum of both numbers
  */
 int add(int num1, int num2)
 {
