f133808cf30edc899355bda7bdfdf34e6a84d01d
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-15 17:24:37 -0500

    Update 4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -9,12 +9,14 @@ int _isalpha(int c)
 {
 char comparison;
 
-for (comparison = 'a'; comparison <= 'z'; comparison++){
-if ( c == comparison )
+for (comparison = 'a'; comparison <= 'z'; comparison++)
+{
+if (c == comparison)
 return (1);
 }
-for (comparison = 'A'; comparison <= 'Z'; comparison++){
-if ( c == comparison )
+for (comparison = 'A'; comparison <= 'Z'; comparison++)
+{
+if (c == comparison)
 return (1);
 }
 return (0);
