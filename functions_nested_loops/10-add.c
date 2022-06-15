dba6b498ea3824093b1f7b0ff710f877d674808c
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-15 16:21:57 -0500

    Update 10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * add - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @num1: The number to begin counting at.
+ * @num2: The number to begin counting at.
+ */
 int add(int num1, int num2)
 {
 	return (num1 + num2);
