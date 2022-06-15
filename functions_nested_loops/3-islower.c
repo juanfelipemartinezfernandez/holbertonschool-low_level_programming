b2512450238e07f78fc1d8634998fc7196f66651
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-15 17:18:33 -0500

    Update 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * _islower - shows if it is a lower case letter
+ *_islower - shows if it is a lower case letter
  *@c: letter to evaluate
- * Return: 1 if lower case 0 if not
+ *Return: 1 if lower case 0 if not
  */
 int _islower(int c)
 {
 char comparison;
 
-for (comparison = 'a'; comparison <= 'z'; comparison++){
+for (comparison = 'a'; comparison <= 'z'; comparison++)
+{
 if ( c == comparison )
 return (1);
 }
