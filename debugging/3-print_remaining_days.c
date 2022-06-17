38ee6aed764eee9221b7367b0e336c1697e1125e
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-16 22:39:17 -0500

    Update 3-print_remaining_days.c

--- debugging/3-print_remaining_days.c
+++ debugging/3-print_remaining_days.c
@@ -17,7 +17,10 @@ void print_remaining_days(int month, int day, int year)
       
 
         printf("Day of the year: %d\n", day);
-        
+          if (month >= 2 && day >= 60)
+        {
+            day++;
+        }
         printf("Remaining days: %d\n", 366 - day);
     }
     else
