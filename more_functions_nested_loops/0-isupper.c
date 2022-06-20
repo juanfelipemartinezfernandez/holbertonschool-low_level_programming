44461166a345dd1dd502b0acf6ae937e5559d38b
Juan Felipe <juanfelipe.martinez.fernandez@gmail.com>
2022-06-20 06:51:01 -0700

    Added change

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int _isupper(int c){
+		for (char ch = 'A'; ch <= 'Z'; ch++)
+		{
+			if (c == ch)
+				return (1);
+		}
+		
+
+
+
+	}
+	return (0);
+}	
