cec7b1542e644053ca74ac545797cd87425ba61c
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-20 09:35:46 -0500

    added

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - will check if parameter given is digit
+ * @c: number to compare
+ * Return: 1 if digit, 0 if something else
+ */
+int _isdigit(int c)
+{
+        int n;
+
+        for (n = 0; n < 10; n++)
+        {
+                if (c == n)
+                        return (1);
+        }
+        return (0);
+
+}
