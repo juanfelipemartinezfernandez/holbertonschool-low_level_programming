999c9075739fd1dc34db706aa8147c6ad47cf59a
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-20 18:15:57 -0500

    Update 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,6 +3,20 @@
 void print_triangle(int size)
 {
 
+int i, j = 0, k;
+
+for (i = 0; i < size; i++)
+{
+        for (j = i; j < (size - 1); j++)
+        {
+                _putchar(' ');
+        }
+        for ( k = (size - 1); k < size; k++)
+        {
+                _putchar('#');
+        }
+        _putchar('\n');
+}
 
 
 }
