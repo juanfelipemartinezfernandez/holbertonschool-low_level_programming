280c11631b1620f4f69faa1860d72aad6ceb0c6c
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-20 10:11:17 -0500

    added

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - WIll print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+int c;
+
+for (c = 0; c < 10; c++)
+{
+_putchar(c);
+_putchar('\n');
+}
+}
