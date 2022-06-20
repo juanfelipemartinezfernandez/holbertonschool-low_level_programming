e52fdf7815066cc6271008cf778e8cfc9caa03a4
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-20 13:49:15 -0500

    Update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,20 @@ int i, j;
 
 if (n > 0)
 {
-        for (i = 0; i < n; i++)
-        {       
-                j = 0;
-        
-                while (j < i)
-                {
-                        _putchar(' ');
-                        j++;
-                }
-                _putchar('\\');
-                _putchar('\n');
-        }
+for (i = 0; i < n; i++)
+{       
+j = 0;
+
+while (j < i)
+{
+_putchar(' ');
+j++;
+}
+_putchar('\\');
+if (i == n - 1)
+	continue;
+_putchar('\n');
+}
 }
 _putchar('\n');
 
