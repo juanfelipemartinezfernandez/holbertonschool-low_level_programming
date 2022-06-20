feefd50320ff1c20ef10739fb61e4e41befdc943
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-20 09:37:53 -0500

    Update 1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -11,7 +11,7 @@ int _isdigit(int c)
 
         for (n = 0; n < 10; n++)
         {
-                if (c == n)
+                if (n == c)
                         return (1);
         }
         return (0);
