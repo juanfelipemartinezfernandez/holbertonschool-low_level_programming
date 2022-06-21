3dead40c9163649a7b227561113f4eded5dd8a39
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-20 19:37:11 -0500

    Update 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_triangle - will print a triangle
+ * 
+ * @size: will define the hight of the triangle
+ */
 void print_triangle(int size)
 {
 
@@ -7,21 +11,14 @@ int i, j = 0, k;
 
 if (size > 0)
 {
-
 for (i = 0; i < size; i++)
 {
-        for (j = i; j < (size - 1); j++)
-        {
-                _putchar(' ');
-        }
-        
-        for (k = 0; k < (i + 1); k++)
-		_putchar('#');
-        
-        
-        _putchar('\n');
+for (j = i; j < (size - 1); j++)
+_putchar(' ');
+for (k = 0; k < (i + 1); k++)
+_putchar('#');
+_putchar('\n');
 }
-
 }
 else
 _putchar('\n');
