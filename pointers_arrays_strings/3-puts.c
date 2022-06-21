2b2480df30346bcdf554933c77fd0010aa0fcfa3
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-21 18:12:14 -0500

    added

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,22 @@
-void _puts(char *str);
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str the string to print out
+ */
+void _puts(char *str)
+{
+char *string[] = *str;
+int a = 1, i = 0;
+
+while (a == 1)
+{
+if (string[i] != '\0')
+{
+_putchar(string[i]);
+}
+else
+_putchar('\n');
+}
+
+
+}
