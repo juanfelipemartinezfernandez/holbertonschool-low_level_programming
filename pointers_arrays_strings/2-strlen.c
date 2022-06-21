39817426640ebb39df18c91470ad563eb22f60e3
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-21 17:43:25 -0500

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,18 @@
-int _strlen(char *s);
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string to analize
+ * Return: it returns the lenght of the string
+ */
+int _strlen(char *s)
+{
+int contador = 0;
+
+while (1)
+{
+if (s[contador] != "\\0")
+contador++;
+else
+0;
+}
+}
