7ca6587fceb9ef8c8e5058eb418961cc56d4324b
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-21 16:58:01 -0500

    Update 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value it points to to 98
-* @n: contains the address of the var to change 
+ * @n:contains the address of the var to change
  */
 void reset_to_98(int *n)
 {
