da12781d15de78f2a06a7930a87c82757fd554d5
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-21 21:58:08 -0500

    Update 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -14,22 +14,21 @@ int index = 0, con_s = 0, con_d = 0, i;
 while (src[index])
 {
 dest[index] = src[index];
-index++;     
-con_s++;           
+index++;
+con_s++;
 }
 while (dest[index++])
 {
-con_d++;  
+con_d++;
 }
-if(con_d > con_s)
+if (con_d > con_s)
 {
 int hold = con_d + con_s;
 con_d = hold - con_d;
-for ( i = con_d; i <= con_s; i++)
+for (i = con_d; i <= con_s; i++)
 {
 dest[i] = '\0';
 }
-
 }
 
 return (dest);
