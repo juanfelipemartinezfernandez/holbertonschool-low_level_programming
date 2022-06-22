aa5b0bae5245d73c7b66f332d9594a0b7e26c7f4
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-22 12:08:52 -0500

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,4 +5,19 @@
  * @src: source string
  * Return: String concatenated
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
+{
+int i, len = 0;
+
+while (src[i++])
+{
+len++;
+}
+
+for ( i = 0; i <= len; i++)
+{
+        dest[i] = src[i];
+}
+dest[i] = '\0';
+
+}
