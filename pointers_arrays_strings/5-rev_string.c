0a998c78878f5d4538a631748bea1a1dbb3b9de7
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-21 19:01:58 -0500

    Update 5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,31 @@
-void rev_string(char *s);
+#include "main.h"
+/**
+ * rev_string - function that prints a string, in reverse,
+ * followed by a new line.
+ * @s:
+ */
+
+void rev_string(char *s)
+{
+int a = 1, i = 0, c;
+
+while (a == 1)
+{
+if (*(s + i) != '\0')
+i++;
+else
+{
+a = 0;
+}
+}
+for (c = i; c >= 0; c--)
+{
+_putchar(s[c]);
+if (c == 0)
+{
+continue;
+_putchar('\n');
+
+}
+}
+}
