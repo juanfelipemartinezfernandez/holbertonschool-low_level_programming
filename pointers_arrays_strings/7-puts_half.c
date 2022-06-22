286aeb6746f4ad700684492b9339e975c937da59
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-21 20:50:31 -0500

    Update 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,22 +5,17 @@
  */
 void puts_half(char *str)
 {
-int i = 0, len = 0;
+int i = 0, len = 0, n;
 
 while (str[i++])
 {
 len++;
 }
 if ((len % 2) != 0)
-{
-        len-=1;
-}
-
-for (i = (len / 2); i <= len - 1; i++)
-{
+n = (len - 1) / 2
+else
+n = len / 2;
+for (i = n; i <= len - 1; i++)
 _putchar(str[i]);
-
-}
 _putchar('\n');
-
 }
