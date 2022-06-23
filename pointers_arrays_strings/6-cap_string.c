7c76e4ee3cd65b042abdf5005efd5f8a4010f8b8
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-23 01:34:20 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,4 +3,25 @@
  * cap_string - function that capitalizes all words of a string
  * Return: string with capitalizes all words
  */
-char *cap_string(char *);
+char *cap_string(char *s)
+{
+char i, *p, j;
+int index = 0;
+p = s;
+while (s[index])
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+        if (*p == i && *(p-1) != j)
+        {
+        *p = *p + 32;
+        }
+}
+}
+index++;
+p++;
+}
+return (s);
+}
