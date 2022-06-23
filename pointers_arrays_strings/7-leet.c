03c01ffa7bdaf7d388665dafc50e0112c0eae7b4
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-23 10:02:07 -0500

    Update 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,7 +7,7 @@
 char *leet(char *s)
 {
 int k[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
-char c[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+unsigned char c[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 char i, len = 0, j;
 
 while (s[len])
