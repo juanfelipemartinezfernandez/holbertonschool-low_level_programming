edde388edfdbffaba30dbea58e1663ecf32a4614
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-22 22:12:45 -0500

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,12 @@ int _strcmp(char *s1, char *s2)
 {
 int len1 = 0, len2 = 0;
 
-while (s1[len1])
+while (s1[len1++])
 {
-len1++;
 s1++;
 }
-while (s2[len2])
+while (s2[len2++])
 {
-len2++;
 s2++;
 }
 return (*s1-*s2);
