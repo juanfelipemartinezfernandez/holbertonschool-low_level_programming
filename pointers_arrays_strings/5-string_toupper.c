58e89adee037639d7b21844095118c02c88dd03f
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-23 00:44:52 -0500

    Update 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,4 +3,21 @@
  * *string_toupper changes all lowercase letters of a string to uppercase.
  * Return: lowercase letters of a string
  */
-char *string_toupper(char *);
+char *string_toupper(char *s)
+{
+char i;
+while (s)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+        if (*s == i)
+        {
+               *s = *s - 32;
+        }
+}
+s++;      
+        
+}
+return (s);
+
+}
