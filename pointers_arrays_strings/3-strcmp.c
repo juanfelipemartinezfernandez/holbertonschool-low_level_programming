e68cb626c850e471639c2f3aca6c571044bb3260
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-22 22:05:55 -0500

    add

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,4 +6,20 @@
  * Return: integer less than, equal to, or greater than zero if  s1  is
  * found, respectively, to be less than, to match, or be greater than s2.
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
+{
+int len1 = 0, len2 = 0;
+
+while (s1[len1])
+{
+len1++;
+}
+while (s2[len2])
+{
+len2++;
+}
+return (len1-len2);
+
+
+
+}
