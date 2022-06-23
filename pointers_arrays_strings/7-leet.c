cd3f1b1ce60a652edd96d1f41553a491e673637d
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-23 09:59:43 -0500

    Update 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,6 +1,32 @@
 #include "main.h"
 /**
  * leet - Write a function that encodes a string into 1337.
+ * @s:String to enconde
  * Return: the string encoded
  */
-char *leet(char *);
+char *leet(char *s)
+{
+int k[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+char *c[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+char i, len = 0, j;
+
+while (s[len])
+len++;
+
+
+for (i = 0; i < len; i++)
+{
+        for (j = 0; j < 10; j++)
+        {
+                if (s[i] == c[j])
+                {
+                        s[i] = k[j];
+                }
+                
+        }
+        
+}
+
+return (s);
+
+}
