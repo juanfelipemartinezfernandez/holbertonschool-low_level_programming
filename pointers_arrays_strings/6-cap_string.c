6b7ab9b1bf107dfe7ef4436537fb9037b224e15c
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-23 10:36:36 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -5,27 +5,21 @@
  */
 char *cap_string(char *s)
 {
-char i, *p, j;
-char separator[] = {32, 11, 10, ',', ';', '.', '!', '?', 34, 41, 40, 123, 125};
+char i, *p;
+char separator[] = {32, 11, 10, ',', ';', '.', '!', '?', 34, 41, 40, 123, 125, 0};
 int index = 0, k;
 p = s;
 while (s[index])
 {
 for (i = 'a'; i <= 'z'; i++)
 {
-for (j = 'A'; j <= 'Z'; j++)
-{
-        for (k = 0; k < 13; k++)
+        for (k = 0; k < 14; k++)
         {
-           if ((*p == i && *(p-1) == separator[k]) || (p[0] == i))
+           if ((*p == i && *(p-1) == separator[k]))
         {
         *p = *p - 32;
+        }   
         }
-        
-        
-        
-        }
-}
 }
 index++;
 p++;
