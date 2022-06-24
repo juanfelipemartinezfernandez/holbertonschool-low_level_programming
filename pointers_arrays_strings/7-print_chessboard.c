cc0ba9c1a0c74b08acf6af5528c1bd8e5c2d4a21
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-24 12:39:40 -0500

    Update 7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,23 @@
+#include "main.h"
 /**
- * print_chessboard - 
+ * print_chessboard - prints the chessboard.
  * @a:
  */
 void print_chessboard(char (*a)[8])
 {
+int i, j, len = 0;
+
+while (a[len])
+len++;
+
+for (i = 0; i < len; i++)
+{
+_putchar(a[i]);
+        for (j = 0; j < 8; j++)
+        {
+                _putchar(a[i][j]);
+        }
+        
+}
 
 }
