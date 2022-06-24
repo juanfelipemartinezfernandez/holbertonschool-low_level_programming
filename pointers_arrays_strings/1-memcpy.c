982ed0552982a10a8255830c86818ccf9d95de6e
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-23 23:52:30 -0500

    Update 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,22 @@
 /**
- * _memcpy -
- * @dest:
- * @src:
- * @n:
- * Return: 
+ * _memcpy -  function that copies memory area.
+ * @dest:destination var
+ * @src:source var
+ * @n:n bytes from memory area
+ * Return: destination var with new copied values
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+char *p, *p1;
+unsigned int i;
+p = dest;
+p1 = src;
 
+for (i = 0; i < n; i++)
+{
+        *p = *p1;
+        p++;
+p1++;
+}
+return (dest);
 }
