436707faa42b68906441beb2e3ec788305a8d5e6
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-24 12:57:11 -0500

    Update 7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -13,7 +13,7 @@ for (i = 0; i < a[0]; i++)
 _putchar(a[i][i]);
         for (j = 0; j < 8; j++)
         {
-                _putchar(a[i + 1][j]);
+                _putchar(a[i][j + 1]);
         }
         _putchar('\n');
 }
