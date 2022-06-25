28b547d71d29e32af9a6a532edf0ccd8ff723fac
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2022-06-25 17:36:58 -0500

    Update 8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
 /**
- * print_diagsums -
- * @a:
- * @size:
+ * print_diagsums - return sum of diagnolas of square
+ * @a:matrix
+ * @size:matrix size
  */
 void print_diagsums(int *a, int size)
 {
+int *p;
+p = a;
+int sum, i;
 
+for (i = 0; i < size; i++)
+{
+       
+sum = (a[i]) + (a[i + size]);
+        
+        
+}
+printf("%d ", sum);
 }
